517582e97e14f9455b5f0a0ae3bfa8f0df362a9c
Krishna M <krishna.mahadevaiah@gmail.com>
2022-10-01 07:38:21 +0530

    trial and error

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
 const billamount = document.querySelector("#bill-amount");
 
+// const ver =document.querySelector("#verify")
+// // console.log(billamount.value)
 
-console.log(billamount.value)
 
+// function clickevent()
+// {
+//     console.log(billamount.value)
+// }
+
+// ver.addEventListener(click,clickevent())
+
+
+const checkbutton = console.log(billamount.value);
